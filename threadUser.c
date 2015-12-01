7f85bf29dd59d9d108740bbdde532cc7e8d9c7f1
Richard Hitier <richard.hitier@co-libri.org>
2015-12-01 11:35:35 +0100

    2 threads interlaced

--- threadUser.c
+++ threadUser.c
@@ -2,25 +2,37 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+/*
+ * Linux multi threads tries.
+ *
+ * thank you to Pierre Ficheux:
+ * http://pficheux.free.fr/articles/lmf/threads/
+ */
+
 // g_mutex
 // g_condition
 
 void * cb1(void * arg){
-    printf("in callback cb1 %s\n", (char*)arg);
-    pthread_exit(0);
-}
-
-void * cb2( void *arg){
+    int i;
+    for(i=0; i<5; i++){
+        printf("Thread %s: %d\n", (char*)arg, i);
+        sleep(1);
+    }
     pthread_exit(0);
 }
 
 int main( int argc, char **argv){
-    pthread_t th1;
+    pthread_t th1, th2;
     void *ret;
 
-    if( pthread_create( &th1, NULL, cb1, "hello world")<0){
+    if( pthread_create( &th1, NULL, cb1, "1")<0){
+        perror("THREAD_ERR Couldnt create thread");
+        exit(1);
+    }
+    if( pthread_create( &th2, NULL, cb1, "2")<0){
         perror("THREAD_ERR Couldnt create thread");
         exit(1);
     }
     (void)pthread_join(th1, &ret);
+    (void)pthread_join(th2, &ret);
 }
