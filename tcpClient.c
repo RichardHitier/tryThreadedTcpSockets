df4b06462533a8d7255a9fb64598a356acfcf7a2
Richard Hitier <richard.hitier@co-libri.org>
2015-12-01 17:04:18 +0100

    modularize into smaller functions (client)

--- tcpClient.c
+++ tcpClient.c
@@ -13,45 +13,77 @@ void error(const char *msg)
     exit(0);
 }
 
-int main(int argc, char *argv[])
-{
-    int sockfd, portno, n, i=0;
+void open_and_connect(int *sockfd, char *servername, int portno){
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
+    *sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (*sockfd < 0) 
         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
+
+    server = gethostbyname(servername);
+
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
+
     bzero((char *) &serv_addr, sizeof(serv_addr));
+
     serv_addr.sin_family = AF_INET;
+
     bcopy((char *)server->h_addr, 
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
+
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+
+    if (connect(*sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
+}
+
+void write_on_socket(char *buffer, int sockfd){
+    int n;
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0) 
+         error("ERROR writing to socket");
+}
+
+void read_from_socket(char *buffer, int sockfd){
+    int n;
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
+    if (n < 0) 
+         error("ERROR reading from socket");
+    
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n, i=0;
+
+    char buffer[256];
+
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+
+    portno = atoi(argv[2]);
+
+    open_and_connect(&sockfd, argv[1], portno);
+
     while(1){
         printf("%d Please enter the message: ", i++);
+
         bzero(buffer,256);
         fgets(buffer,255,stdin);
-        n = write(sockfd,buffer,strlen(buffer));
-        if (n < 0) 
-             error("ERROR writing to socket");
-        bzero(buffer,256);
-        n = read(sockfd,buffer,255);
-        if (n < 0) 
-             error("ERROR reading from socket");
+
+        write_on_socket(buffer, sockfd);
+        read_from_socket(buffer, sockfd);
+
+
         printf("I have read: %s\n",buffer);
     }
     close(sockfd);
