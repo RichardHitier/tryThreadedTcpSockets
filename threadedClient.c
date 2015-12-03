4cdbd09c33f4c0b8cbfe8b357d20d5953c99efb8
Richard Hitier <richard.hitier@co-libri.org>
2015-12-03 22:06:50 +0100

    Use a flag var stopping from buffering reception

--- threadedClient.c
+++ threadedClient.c
@@ -1,27 +1,51 @@
 #include <pthread.h>
 #include "clientserverTools.h"
 
+static pthread_mutex_t my_mutex;
 int sockfd;
+int gcondition=1;
 
 void *writing_process(void *arg){
 	int i=0;
     char buffer[256];
     while(1){
-        printf("%d Please enter the message: ", i++);
-
         bzero(buffer,256);
+
+        fprintf(stderr,"%d Please enter the message: ", i++);
+
         fgets(buffer,255,stdin);
+
+        // set gcondition to false
+        pthread_mutex_lock (&my_mutex);
+        gcondition=0;
+        pthread_mutex_unlock (&my_mutex);
 
         write_on_socket(buffer, sockfd);
     }
+    pthread_exit (0);
 }
 
 void *reading_process(void *arg){
     char buffer[256];
+    int count=0;
+    int lcondition;
     while(1){
-        read_from_socket(buffer, sockfd);
-        printf("I have read: %s\n",buffer);
+        lcondition=gcondition;
+        while(lcondition){
+            read_from_socket(buffer, sockfd);
+            count++;
+            // lock doesnt wait
+            pthread_mutex_trylock (&my_mutex);
+            lcondition=gcondition;
+            pthread_mutex_unlock (&my_mutex);
+        }
+        fprintf(stderr,"%d Read\n",count);
+        count=0;
+        pthread_mutex_trylock (&my_mutex);
+        gcondition=1;
+        pthread_mutex_unlock (&my_mutex);
     }
+    pthread_exit (0);
 }
 
 int main(int argc, char *argv[])
@@ -38,6 +62,8 @@ int main(int argc, char *argv[])
     }
 
     portno = atoi(argv[2]);
+
+    pthread_mutex_init (&my_mutex, NULL);
 
     open_and_connect(&sockfd, argv[1], portno);
     pthread_create( &th1, NULL, reading_process, NULL);
