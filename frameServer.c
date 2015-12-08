e404667d44df7baba570febdffb58354505df88f
Richard Hitier <richard.hitier@co-libri.org>
2015-12-08 18:21:19 +0100

    allow body cli argument to server

--- frameServer.c
+++ frameServer.c
@@ -6,11 +6,19 @@ int main(int argc, char *argv[])
 {
     int newsockfd;
     int sockfd, portno;
-    t_msg_frame ourFrame={1,2,3,"hello world",5};
+    t_msg_frame ourFrame={1,2,3,"goodbye cruel world",5};
+    ourFrame.bdyLgth=strlen(ourFrame.msgBody);
 
     if (argc < 2) {
      fprintf(stderr,"ERROR, no port provided\n");
      exit(1);
+    }
+
+    if ( argc > 2){
+        // todo: kindof msgBodyCpy()
+        ourFrame.msgBody=malloc(strlen(argv[2]));
+        strcpy(ourFrame.msgBody, argv[2]);
+        ourFrame.bdyLgth=strlen(ourFrame.msgBody);
     }
 
     portno = atoi(argv[1]);
