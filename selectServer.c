4f09e97deaec5c0faae207fcdea6156e96bc542c
Richard Hitier <richard.hitier@co-libri.org>
2015-12-17 12:11:20 +0100

    loop on read

--- selectServer.c
+++ selectServer.c
@@ -19,36 +19,36 @@ int main(int argc, char *argv[])
 
      get_client_connection(&newsockfd, sockfd);
 
-     do{
-         FD_ZERO(&readset);
-         FD_SET(newsockfd, &readset);
-         result = select( newsockfd + 1, &readset, NULL, NULL, NULL);
-     }while(result==-1&&errno==EINTR);
+     for(;;){
+         do{
+             FD_ZERO(&readset);
+             FD_SET(newsockfd, &readset);
+             result = select( newsockfd + 1, &readset, NULL, NULL, NULL);
+         }while(result==-1&&errno==EINTR);
 
-     if ( result > 0){
-         if( FD_ISSET( newsockfd, &readset)){
-             result=read_from_socket(buffer, newsockfd);
-             if (result == 0) {
-                 /* This means the other side closed the socket */
-                 perror("client closed, we close");
-                 close(newsockfd);
+         if ( result > 0){
+             if( FD_ISSET( newsockfd, &readset)){
+                 result=read_from_socket(buffer, newsockfd);
+                 if (result == 0) {
+                     /* This means the other side closed the socket */
+                     perror("client closed, we close");
+                     close(newsockfd);
+                 }
+                 else {
+                     /* I leave this part to your own implementation */
+                     printf("Here is the message: %s\n",buffer);
+                 }
              }
-             else {
-                 /* I leave this part to your own implementation */
-                 printf("Here is the message: %s\n",buffer);
-             }
+         } else if ( result <0){
+             sprintf( errStr, "Error on select(): %s", strerror(errno));
+             perror(errStr);
          }
-     } else if ( result <0){
-         sprintf( errStr, "Error on select(): %s", strerror(errno));
-         perror(errStr);
      }
 
 
 
 
 //     while( 1 ){
-//         read_from_socket(buffer, newsockfd);
-//         printf("Here is the message: %s\n",buffer);
 //
 //         write_on_socket("I got your message", newsockfd);
 //     }
